53ef96889b5cde2a56658ebc0e46ada595fdf3ff
eltinawhitaker <eltinadublin@yahoo.com>
2018-11-23 12:54:54 -0600

    Commit changes for Lesson12, whitaker

--- src/main/java/edu/dcccd/trans/controller/TransactionController.java
+++ src/main/java/edu/dcccd/trans/controller/TransactionController.java
@@ -1,7 +1,8 @@
 package edu.dcccd.trans.controller;
 
 
-import edu.dcccd.trans.repository.SingletonTransaction;
+
+
 import edu.dcccd.trans.entity.Transaction;
 import edu.dcccd.trans.service.TransactionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 import java.time.DayOfWeek;
 import java.util.Arrays;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import javax.persistence.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,10 +41,9 @@ public class TransactionController {
 
     @GetMapping("/transaction")
     public String loadTransactionPage(Model model){
-        List<Transaction> transactions = transactionService.getAllTransaction();
         model.addAttribute("transactionForm", new Transaction());
         model.addAttribute("days",getDays());
-        model.addAttribute("transactions", transactions);
+        model.addAttribute("transactions", transactionService.getAllTransaction());
         return "transaction";
     }
 
@@ -59,7 +64,10 @@ public class TransactionController {
             model.addAttribute("errors", errors);
             return "transaction";
         }
-        transaction.setId(SingletonTransaction.getInstance().autoIncrementID+=1);
+        LocalDateTime now = LocalDateTime.now();
+        String time = now.format(DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM));
+        transaction.setTime(time);
+
         transactionService.createTransaction(transaction);
         return "redirect:transaction";
     }
